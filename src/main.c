00f8de0fb441fead8fda79184f191f6f94505ea3
sudaniel <sudaniel@student.42heilbronn.de>
2025-01-15 15:59:20 +0100

    resolve confilict

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sudaniel <sudaniel@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 08:18:57 by sudaniel          #+#    #+#             */
-/*   Updated: 2025/01/15 15:48:15 by sudaniel         ###   ########.fr       */
+/*   Updated: 2025/01/15 15:56:21 by sudaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,6 @@
 
 static char	*prompt(void)
 {
-<<<<<<< HEAD
-    char *res;
-    printf("TEST Minishell\n");
-    int x= 123456;
-
-    res = ft_itoa(x);
-    printf("Test Libft: %s\n", res);
-    return (0);
-=======
 	char	*user;
 	char	*line;
 
@@ -45,5 +36,4 @@ int	main(void)
 		ft_printf("%s\n", line);
 	}
 	return (0);
->>>>>>> f8d83398941d9cb18b6fba54f185b561514e37d5
 }
